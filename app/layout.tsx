22a65fad978bccf6befedf53cac5abd7dadb0edd
18-dhruv <dhryvddogra6325@gmail.com>
2025-06-10 13:10:18 +0530

    Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className='bg-black text-white'>
       <body>{children}</body>
     </html>
   )
